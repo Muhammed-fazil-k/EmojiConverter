31b90320e31d49fbe79a6a9d55b4a3393e3d31a6
samuel-abhishek <samuelabhishekamm@gmail.com>
2021-08-14 12:49:06 +0530

    initial commit

---
+++ app/src/main/java/edu/texttoemoji/view/DashboardView.java
@@ -0,0 +1,13 @@
+package edu.texttoemoji.view;
+
+import android.widget.EditText;
+
+/**
+ * Created by Ankit on 20/11/17.
+ */
+
+public interface DashboardView {
+
+    String convertEmoji();
+
+}
